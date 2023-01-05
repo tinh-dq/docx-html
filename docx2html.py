86ba7485ac98d0ab2b20ddee879db1d4c21d5530
Doan Quang Tinh <65673146+tinh-dq@users.noreply.github.com>
2023-01-06 01:41:29 +0700

    Add files via upload

---
+++ docx2html.py
@@ -0,0 +1,32 @@
+import os
+import mammoth as mammoth
+from glob import glob
+
+
+def save(texts, file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.writelines(texts)
+
+
+def createFolder(path):
+    is_exist = os.path.exists(path)
+    if not is_exist:
+        # Create a new directory because it does not exist
+        os.makedirs(path)
+        print("The new directory is created!")
+
+
+x = glob("D:\\*.docx")
+form = """<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    {text}
+</body>
+</html>"""
+for f in x:
+    with open(f, "rb") as docx_file:
+        result = mammoth.convert_to_html(docx_file)
+        text = form.format(text=result.value)
+        save(text, f.replace("docx", "html"))
